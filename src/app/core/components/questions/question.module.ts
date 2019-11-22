d50d6afbc1567f8ee31fd4302f0f3c1d9a94275a
Viktor Kmin <kminvictor@gmail.com>
2019-11-22 16:45:44 +0200

    Update question.module.ts

--- src/app/core/components/questions/question.module.ts
+++ src/app/core/components/questions/question.module.ts
@@ -1,16 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatCardModule} from '@angular/material';
+import {MatCardModule, MatAutocompleteModule} from '@angular/material';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import {FormModule, MaterialModule} from '../../../shared/modules';
 import {QuestionRoutingModule} from './question-routing.module';
 import {QuestionPipe} from '../../../shared/pipe/question.pipe';
-
 import {InfoHelperComponent} from './info-helper/info-helper.component';
 import {AddQuestionFormComponent} from './add-question-form/add-question-form.component';
-import {MatExpansionModule} from '@angular/material/expansion';
 import { FilterForQuestionsComponent } from './filter-for-questions/filter-for-questions.component';
-import {MatAutocompleteModule} from '@angular/material';
 import {FilterPipe} from './filter.pipe';
 import {QuestionsLayoutComponent} from './questions-layout/questions-layout.component';
 
