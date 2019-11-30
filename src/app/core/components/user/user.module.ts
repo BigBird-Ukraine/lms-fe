ed3d4cc79450494dd4e76b225730b9ab52ba463e
VictorKmin <kminvictor@gmail.com>
2019-11-30 13:48:38 +0200

    Remove regexp to constants

--- src/app/core/components/user/user.module.ts
+++ src/app/core/components/user/user.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
+
 import {RegistrationComponent} from './registration/registration.component';
 import {UserRoutingModule} from './user-routing.module';
-import {MatButtonModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
-import {ReactiveFormsModule} from '@angular/forms';
-
-
 
 @NgModule({
   declarations: [
---
+++ src/app/core/constans/regexp.ts
@@ -0,0 +1,4 @@
+export const regExp = {
+  nameRegexp: '^[a-zA-Z]*$',
+  passwordRegexp: '^(?!.* )(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[@$!%*#?&]).{8,}$'
+}
