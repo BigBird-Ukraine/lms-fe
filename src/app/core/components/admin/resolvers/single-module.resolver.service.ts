7feaa174a0a16fcb68cef31c7605a1c605e3aa2d
Oleh Kosylo <kosylopoker@gmail.com>
2020-08-11 17:25:04 +0300

    [FE][MAIN] LMS-187 Get user groups on the user page

--- src/app/core/components/admin/resolvers/single-module.resolver.service.ts
+++ src/app/core/components/admin/resolvers/single-module.resolver.service.ts
@@ -1,13 +1,13 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRoute, ActivatedRouteSnapshot, Params, Resolve, RouterStateSnapshot} from '@angular/router';
-import {IFullModule, IModule} from '../interfaces';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {IFullModule} from '../interfaces';
 import {Observable} from 'rxjs';
 import {AdminModuleService} from '../services';
 
 @Injectable()
 export class SingleModuleResolverService implements Resolve<IFullModule> {
 
-  constructor(private aModuleService: AdminModuleService, private route: ActivatedRoute) {
+  constructor(private aModuleService: AdminModuleService) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IFullModule> | Promise<IFullModule> | IFullModule {
